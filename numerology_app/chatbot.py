96ddb158a7051a883b68d67adf5d4690775b8b8c
shaq31415926 <sarah.haq11@gmail.com>
2024-12-10 11:56:32 +0100

    numerology app code

---
+++ numerology_app/chatbot.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from hugchat import hugchat
+from hugchat.login import Login
+
+# App title
+@st.cache_resource
+def connect_to_hugging_face():
+    hf_email = st.secrets['email']
+    hf_pass = st.secrets['password']
+
+    # connect to hugging face
+    sign = Login(hf_email, hf_pass)
+    cookies = sign.login()
+
+    return cookies
+
+def generate_response(prompt):
+    cookies = connect_to_hugging_face()
+    # Create ChatBot
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    return chatbot.chat(prompt)
+
+
