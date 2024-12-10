b968cf7343538ead7a5c2aba08a9036ab01632f8
shaq31415926 <sarah.haq11@gmail.com>
2024-12-10 15:02:16 +0100

    added help comments

--- numerology_app/numerology_app.py
+++ numerology_app/numerology_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from calculate_numbers import calculate_life_path_number, calculate_personality_number, calculate_soul_number, calculate_destiny_number
+from calculate_numbers import calculate_life_path_number, calculate_personality_number, calculate_soul_number, \
+    calculate_destiny_number
 from chatbot import generate_response
 
 # set the page config info
@@ -9,7 +10,8 @@ st.set_page_config(
 
 # welcome message
 st.title("Welcome to your very own Numerology Assistant")
-st.markdown("If you would like to learn more about numerology you can check this Spotify Podcast [here](https://open.spotify.com/episode/78w5hXBfHnzFTVHC3aQv3c).")
+st.markdown(
+    "If you would like to learn more about numerology you can check this Spotify Podcast [here](https://open.spotify.com/episode/78w5hXBfHnzFTVHC3aQv3c).")
 
 # set button counters
 if 'lifepath_count' not in st.session_state:
@@ -24,11 +26,13 @@ if 'soul_count' not in st.session_state:
 if 'destiny_count' not in st.session_state:
     st.session_state.destiny_count = 0
 
+
 def activate_lifepath():
     st.session_state.lifepath_count = 1
     st.session_state.personality_count = 0
     st.session_state.destiny_count = 0
     st.session_state.soul_count = 0
+
 
 def activate_personality():
     st.session_state.lifepath_count = 0
@@ -36,17 +40,20 @@ def activate_personality():
     st.session_state.destiny_count = 0
     st.session_state.soul_count = 0
 
+
 def activate_soul():
     st.session_state.lifepath_count = 0
     st.session_state.personality_count = 0
     st.session_state.destiny_count = 0
     st.session_state.soul_count = 1
 
+
 def activate_destiny():
     st.session_state.lifepath_count = 0
     st.session_state.personality_count = 0
     st.session_state.destiny_count = 1
     st.session_state.soul_count = 0
+
 
 def display_prompt_result():
     with st.spinner("Thinking..."):
@@ -58,24 +65,30 @@ with st.chat_message("assistant"):
     st.write("Hello Human👋! What would you like me to calculate?")
     c1, c2 = st.columns(2)
     with c1:
-        life_path = st.button("Life Path Number", on_click=activate_lifepath)
-        personality_number = st.button("Personality Number", on_click=activate_personality)
+        life_path = st.button("Life Path Number",
+                              on_click=activate_lifepath,
+                              help="This number represents who you are at your core")
+        personality_number = st.button("Personality Number",
+                                       on_click=activate_personality,
+                                       help="This is the mask you present to others"
+                                       )
     with c2:
-        destiny_number = st.button("Destiny Number", on_click=activate_destiny)
-        soul_number = st.button("Soul Number", on_click=activate_soul)
-
+        destiny_number = st.button("Destiny Number",
+                                   on_click=activate_destiny,
+                                   help="This number unveils your life's purpose")
+        soul_number = st.button("Soul Number",
+                                on_click=activate_soul,
+                                help="This number represents your inner most desires")
 
 if st.session_state.lifepath_count == 1:
     with st.chat_message("assistant"):
         dob = st.date_input("When's your birthday", value=None)
         if dob:
-           life_path_number = calculate_life_path_number(dob)
-           st.write(f"Your Life Path is {life_path_number}")
-           prompt = st.chat_input(f"Would you like to learn more about your Life Path number {life_path_number}")
-           if prompt:
-               display_prompt_result()
-
-
+            life_path_number = calculate_life_path_number(dob)
+            st.write(f"Your Life Path is {life_path_number}")
+            prompt = st.chat_input(f"Would you like to learn more about your Life Path number {life_path_number}")
+            if prompt:
+                display_prompt_result()
 
 if st.session_state.personality_count == 1 or st.session_state.soul_count == 1 or st.session_state.destiny_count == 1:
     with st.chat_message("assistant"):
@@ -84,7 +97,8 @@ if st.session_state.personality_count == 1 or st.session_state.soul_count == 1 o
             # st.write(f"Your full name is {name}")
             if st.session_state.personality_count == 1:
                 personality_number = calculate_personality_number(name)
-                prompt = st.chat_input(f"Would you like to learn more about your Personality Number {personality_number}")
+                prompt = st.chat_input(
+                    f"Would you like to learn more about your Personality Number {personality_number}")
             if st.session_state.soul_count == 1:
                 soul_number = calculate_soul_number(name)
                 prompt = st.chat_input(f"Would you like to learn more about your Soul Number {soul_number}")
@@ -94,11 +108,3 @@ if st.session_state.personality_count == 1 or st.session_state.soul_count == 1 o
 
             if prompt:
                 display_prompt_result()
-
-
-
-
-
-
-
-
